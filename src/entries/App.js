4591f684efed64e717e1fd464d3338238258353e
Héctor Reyes <h.reyes@idventiva.com>
2020-06-29 20:56:55 -0500

    COofiguración inicial de carpetas y archivos

--- src/entries/App.js
+++ src/entries/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './img/catspace.jpg'
-import './App.css';
+import React from "react";
+import logo from "../img/catspace.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="catspace" alt="catspace" />
-        <h1>
-          welcome to comeet-cat
-        </h1>
+        <h1>Welcome to comeet-cat</h1>
       </header>
     </div>
   );
