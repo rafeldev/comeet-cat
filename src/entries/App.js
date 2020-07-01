d2a986cd115829f965d7c3768d19363c1fc03f44
Rafael Alvarez <tecnoportuaria@gmail.com>
2020-07-01 18:51:54 -0500

    incluimos sass dentro de nuestro proyecto

--- src/entries/App.js
+++ src/entries/App.js
@@ -1,13 +1,17 @@
-import React from "react";
-import logo from "../img/catspace.jpg";
-import "../static/css/index.css";
-import "./App.css";
+import React from 'react';
+import logo from '../img/catspace.jpg';
+
+/* --styles-- */
+import '../static/sass/font.scss';
+import '../static/sass/cat.scss';
+
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="catspace" alt="catspace" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='catspace' alt='catspace' />
         <h1>Welcome to comeet-cat</h1>
       </header>
     </div>
