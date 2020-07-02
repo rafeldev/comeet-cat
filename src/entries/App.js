4b18af8e28b9a6c0a04f2a901d31526fee04db31
Rafael Alvarez <tecnoportuaria@gmail.com>
2020-07-02 10:53:18 -0500

    instalacion de react-router y de iconos

--- src/entries/App.js
+++ src/entries/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from '../img/catspace.jpg';
+import { Link } from 'react-router-dom';
 
 /* --styles-- */
-import '../static/sass/font.scss'
-
+import '../static/sass/font.scss';
+import { AiOutlineGithub } from 'react-icons/ai';
 
 import './App.css';
 
@@ -11,8 +12,13 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <h1>
+          Welcome to comeet-cat
+          <a className='github-icon' href=''>
+            <AiOutlineGithub />
+          </a>
+        </h1>
         <img src={logo} className='catspace' alt='catspace' />
-        <h1>Welcome to comeet-cat</h1>
       </header>
     </div>
   );
