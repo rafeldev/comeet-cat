7c896181d4a405f35dd324d77dcd18731df40d51
Rafael Alvarez <tecnoportuaria@gmail.com>
2020-07-06 19:37:36 -0500

    corrigiendo el position del contenedor de space-betwen a space-around

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Header from '../Components/Header';
+import Hero from '../Components/Hero';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Hero />
       </div>
     );
   }
