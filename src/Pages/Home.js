06db845085a56d13a1985d896bd34bae93e66eaa
Rafael Alvarez <tecnoportuaria@gmail.com>
2020-07-09 13:04:26 -0500

    actualizando el entregable con titulo y un hr

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,14 +5,27 @@ import LastAsteroids from '../Components/LastAsteroids';
 import DataTransformContainer from '../Components/DataTransformContainer';
 import Hero from '../Components/Hero';
 
+import '../index.scss';
+
 class Home extends Component {
   render() {
     return (
       <div>
+        <h2 className='h2-prueba'>Header Container</h2>
         <Header />
+        <hr className='hr-prueba' />
+
+        <h2 className='h2-prueba'>Hero Container</h2>
         <Hero />
+        <hr className='hr-prueba' />
+
+        <h2 className='h2-prueba'>Data transform container</h2>
         <DataTransformContainer />
+        <hr className='hr-prueba' />
+
+        <h2 className='h2-prueba'>Last Asteroids</h2>
         <LastAsteroids />
+        <hr className='hr-prueba' />
       </div>
     );
   }
