487bb4fae6531824eb38adb257b34b1ec9669a92
Héctor Reyes <h.reyes@idventiva.com>
2020-07-11 14:13:54 -0500

    Merge Ready

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
-import FeaturedContainer from "./containers/FeaturedContainer";
-import ColaboratorContainer from "./containers/ColaboratorsContainer";
-import CallToAction from "./containers/CallToAction";
-import DetailsContainer from "./containers/DetailsContainer";
 
 import "../index.css";
 
@@ -13,24 +9,8 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h2 className="h2-prueba">Header Container</h2>
         <Header />
-        <hr />
-        <h2 className="h2-prueba">Featured Container</h2>
-        <FeaturedContainer />
-        <hr />
-        <h2 className="h2-prueba">Colaborators Container & Card</h2>
-        <ColaboratorContainer />
-        <hr />
-        <h2 className="h2-prueba">Call to Action</h2>
-        <CallToAction />
-        <hr />
-        <h2 className="h2-prueba">Details Container & Info</h2>
-        <DetailsContainer />
-        <hr />
-        <h2 className="h2-prueba">Footer</h2>
         <Footer />
-        <hr />
       </div>
     );
   }
