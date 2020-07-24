651662753072cd0c2fe17b67f710bf8db6d475b8
Héctor Reyes <h.reyes@idventiva.com>
2020-07-24 18:58:45 -0500

    Detalles en Footer y funcionalidades

--- src/Components/molecules/Featured.js
+++ src/Components/molecules/Featured.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 
 import "../../static/sass/SassComponents/Featured.scss";
-import FeaturedImg from "../../img/heroImg.svg";
 
 class Featured extends Component {
   render() {
     return (
       <div className="featured">
-        <img src={FeaturedImg} alt="feature icon" />
+        <img src={this.props.img} alt="feature icon" />
         <div className="featured__info">
           <h3 className="featured__title">{this.props.title}</h3>
           <p className="featured__description">{this.props.description}</p>
