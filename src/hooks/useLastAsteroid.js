1786ae460a0bf938941ec88c0d7f8651221a04c2
soyraffaello <tecnoportuaria@gmail.com>
2020-07-27 20:19:07 -0500

    ignorame

--- src/hooks/useLastAsteroid.js
+++ src/hooks/useLastAsteroid.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export const useLastAsteroid = (getNeos) => {
     return(
-        let arrayNeo = getNeos.slice(0, 3).map((neo) => {
+        arrayNeo = getNeos.slice(0, 3).map((neo) => {
           const id = Math.round(Math.random() * ImgData.length);
           neo.sourceimg = ImgData[id];
       
           return neo;
-        });
+        })
       
-        return arrayNeo;
 
     )
+    return arrayNeo;
 };
