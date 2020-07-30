7a4fd5a44fc81f83c5e445faa29cedc23c568243
rafeldev <tecnoportuaria@gmail.com>
2020-07-30 17:24:21 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/comeet-cat/precache-manifest.8a93a587739ef9671edbf8dcd0deab46.js"
+  "/comeet-cat/precache-manifest.abbd7685c1cc726c3fdd890c19e35ba3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/comeet-cat/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
